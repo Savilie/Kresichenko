2f4af17c254f20fec15027c4967eff5cb4fc9f4f
danya <danyakresichenko0709@gmail.com>
2023-10-07 19:23:04 +0300

    PZ2 done

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -2,11 +2,12 @@
 #Дано трехзначное число. Вывести вначале его последнюю цифру (единицу), а затем - его среднюю цифру (десятки)
 
 
-num = int(input("Введите трехзначное число"))
+num = int(input("Введите трехзначное число: "))
+if num > 99 and num < 1000:
+    last_digit = num % 10
+    middle_digit = (num // 10) % 10
+    print("Последнее цифра", last_digit)
+    print("Средняя цифра", middle_digit)
+else:
+    print("Введите трёхзначное число!")
 
-last_digit = num % 10
-
-middle_digit = (num // 10) % 10
-
-print("Последнее цифра", last_digit)
-print("Средняя цифра", middle_digit)
